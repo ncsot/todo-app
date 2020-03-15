a7ea1f3eb828b261b5a1428c098b9e0288773177
ncsot <ncsot@ya.ru>
2020-03-15 16:43:52 +0300

    Updates

---
+++ precache-manifest.44e78a7cb5507326828b01224a599ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b593e496dde2f3deb0103fac30ad5891",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "93ca1951d6d854ea7286",
+    "url": "/todo-app/static/js/2.aa33fccd.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/todo-app/static/js/2.aa33fccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5489df8612ae666129cd",
+    "url": "/todo-app/static/js/main.12907239.chunk.js"
+  },
+  {
+    "revision": "c4d46ae89b04c6bd74a0",
+    "url": "/todo-app/static/js/runtime-main.fe48852d.js"
+  },
+  {
+    "revision": "8d4f00d17b163e9843a6ec244a246cad",
+    "url": "/todo-app/static/media/logo.8d4f00d1.png"
+  }
+]);
